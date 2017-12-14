767b7148fc7c5bd100a3353d9bd9ab68c6b678cc
Hikari (Aikawa) Kanouchi <hikari@hikar.in>
2017-12-15 00:35:02 +0800

    Initial Commit

---
+++ CSharp-REPL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using Mono.CSharp;
+
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var evaluator = new Evaluator(
+                new CompilerContext(
+                    new CompilerSettings(),
+                    new ConsoleReportPrinter()));
+            evaluator.ReferenceAssembly(Assembly.GetExecutingAssembly());
+            evaluator.Run("using System; using ConsoleApp1;");
+
+            var input = "";
+            while (input != "exit")
+            {
+                Console.Write(">");
+                input = Console.ReadLine();
+
+                if (input.EndsWith(";"))
+                {
+                    var result = evaluator.Run(input);
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    object result;
+                    bool result_set;
+                    var ret = evaluator.Evaluate(input, out result, out result_set);
+                    if (result != null)
+                    {
+                        Console.WriteLine("{0}", result);
+                    }
+                }
+            }
+        }
+    }
+}
