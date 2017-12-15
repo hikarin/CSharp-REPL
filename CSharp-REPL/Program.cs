3c29e2c87d92399282ff28b97a107c647207f2ca
Hikari (Aikawa) Kanouchi <hikari@hikar.in>
2017-12-16 01:31:34 +0800

    proper error handling

--- CSharp-REPL/Program.cs
+++ CSharp-REPL/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Reflection;
 using Mono.CSharp;
+using System.Text;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -8,12 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var evaluator = new Evaluator(
-                new CompilerContext(
-                    new CompilerSettings(),
-                    new ConsoleReportPrinter()));
+            var _stringBuilder = new StringBuilder();
+            var _writer = new StringWriter(_stringBuilder);
+            var settings = new CompilerSettings();
+            var printer = new StreamReportPrinter(_writer);
+            var context = new CompilerContext(settings, printer);
+            var evaluator = new Evaluator(context);
             evaluator.ReferenceAssembly(Assembly.GetExecutingAssembly());
-            evaluator.Run("using System; using ConsoleApp1;");
 
             var input = "";
             while (input != "exit")
@@ -24,16 +27,29 @@ namespace ConsoleApp1
                 if (input.EndsWith(";"))
                 {
                     var result = evaluator.Run(input);
-                    Console.WriteLine(result);
+                    if (printer.ErrorsCount > 0 || printer.WarningsCount > 0)
+                    {
+                        Console.WriteLine(_stringBuilder);
+                        _stringBuilder.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine(result);
+                    }
                 }
                 else
                 {
                     object result;
                     bool result_set;
                     var ret = evaluator.Evaluate(input, out result, out result_set);
-                    if (result != null)
+                    if (printer.ErrorsCount > 0 || printer.WarningsCount > 0)
                     {
-                        Console.WriteLine("{0}", result);
+                        Console.WriteLine(_stringBuilder);
+                        _stringBuilder.Clear();
+                    }
+                    else if (result != null)
+                    {
+                        Console.WriteLine(result);
                     }
                 }
             }
